410dd66127ebb2d2ec0784e81b9a2cde8dbfc5c0
maxim-savchuk <maxsavchuk1105@gmail.com>
2021-04-05 17:25:20 +0300

    fix-mobile-menu
    теперь при нажатии на ссылку меню закрываться и ехать на соответствующую секцию.
    дже ес старый закоментил и добавил новый )))) все найс!

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,26 +1,122 @@
-(() => {
-  const mobileMenu = document.querySelector('.js-menu-container');
-  const openMenuBtn = document.querySelector('.js-open-menu');
-  const closeMenuBtn = document.querySelector('.js-close-menu');
+parcelRequire = (function (e, r, t, n) {
+  var i,
+    o = 'function' == typeof parcelRequire && parcelRequire,
+    u = 'function' == typeof require && require;
+  function f(t, n) {
+    if (!r[t]) {
+      if (!e[t]) {
+        var i = 'function' == typeof parcelRequire && parcelRequire;
+        if (!n && i) return i(t, !0);
+        if (o) return o(t, !0);
+        if (u && 'string' == typeof t) return u(t);
+        var c = new Error("Cannot find module '" + t + "'");
+        throw ((c.code = 'MODULE_NOT_FOUND'), c);
+      }
+      (p.resolve = function (r) {
+        return e[t][1][r] || r;
+      }),
+        (p.cache = {});
+      var l = (r[t] = new f.Module(t));
+      e[t][0].call(l.exports, p, l, l.exports, this);
+    }
+    return r[t].exports;
+    function p(e) {
+      return f(p.resolve(e));
+    }
+  }
+  (f.isParcelRequire = !0),
+    (f.Module = function (e) {
+      (this.id = e), (this.bundle = f), (this.exports = {});
+    }),
+    (f.modules = e),
+    (f.cache = r),
+    (f.parent = o),
+    (f.register = function (r, t) {
+      e[r] = [
+        function (e, r) {
+          r.exports = t;
+        },
+        {},
+      ];
+    });
+  for (var c = 0; c < t.length; c++)
+    try {
+      f(t[c]);
+    } catch (e) {
+      i || (i = e);
+    }
+  if (t.length) {
+    var l = f(t[t.length - 1]);
+    'object' == typeof exports && 'undefined' != typeof module
+      ? (module.exports = l)
+      : 'function' == typeof define && define.amd
+      ? define(function () {
+          return l;
+        })
+      : n && (this[n] = l);
+  }
+  if (((parcelRequire = f), i)) throw i;
+  return f;
+})(
+  {
+    Z6FK: [
+      function (require, module, exports) {
+        !(function () {
+          var e = {
+            openMenuBtn: document.querySelector('[js-open-menu]'),
+            closeMenuBtn: document.querySelector('[js-close-menu]'),
+            menu: document.querySelector('[js-menu-container]'),
+            menuLink1: document.querySelector('[first-link]'),
+            menuLink2: document.querySelector('[second-link]'),
+            menuLink3: document.querySelector('[third-link]'),
+            menuLink4: document.querySelector('[fourth-link]'),
+            menuLink5: document.querySelector('[fifth-link]'),
+          };
+          function n() {
+            document.body.classList.toggle('js-open-menu'),
+              e.menu.classList.toggle('is-open'),
+              document.body.classList.toggle('menu-open');
+          }
+          e.openMenuBtn.addEventListener('click', n),
+            e.closeMenuBtn.addEventListener('click', n),
+            e.menuLink1.addEventListener('click', n),
+            e.menuLink2.addEventListener('click', n),
+            e.menuLink3.addEventListener('click', n),
+            e.menuLink4.addEventListener('click', n),
+            e.menuLink5.addEventListener('click', n);
+        })();
+      },
+      {},
+    ],
+  },
+  {},
+  ['Z6FK'],
+  null,
+);
 
-  const toggleMenu = () => {
-    const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenu.classList.toggle('is-open');
+// (() => {
+//   const mobileMenu = document.querySelector('.js-menu-container');
+//   const openMenuBtn = document.querySelector('.js-open-menu');
+//   const closeMenuBtn = document.querySelector('.js-close-menu');
 
-    const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
-  };
+//   const toggleMenu = () => {
+//     const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+//     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+//     mobileMenu.classList.toggle('is-open');
 
-  openMenuBtn.addEventListener('click', toggleMenu);
-  closeMenuBtn.addEventListener('click', toggleMenu);
+//     const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
+//     bodyScrollLock[scrollLockMethod](document.body);
+//   };
 
-  // Закрываем мобильное меню на более широких экранах
-  // в случае изменения ориентации устройства.
-  window.matchMedia('(min-width: 1344px)').addEventListener('change', e => {
-    if (!e.matches) return;
-    mobileMenu.classList.remove('is-open');
-    openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
-  });
-})();
+//   openMenuBtn.addEventListener('click', toggleMenu);
+//   closeMenuBtn.addEventListener('click', toggleMenu);
+
+//   // Закрываем мобильное меню на более широких экранах
+//   // в случае изменения ориентации устройства.
+//   window.matchMedia('(min-width: 1344px)').addEventListener('change', e => {
+//     if (!e.matches) return;
+//     mobileMenu.classList.remove('is-open');
+//     openMenuBtn.setAttribute('aria-expanded', false);
+//     bodyScrollLock.enableBodyScroll(document.body);
+//   });
+// })();
